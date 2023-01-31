da6ac5d8ae20adf5d9306b6fc0c53361d642bae7
CatarinaK1 <111956032+CatarinaK1@users.noreply.github.com>
2023-01-31 11:56:31 +0200

    Create ex1.php

---
+++ ex1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Php</title>
+</head>
+<body>
+    <h1> My simple PHP script </h1>
+    <?php
+        //Opening tag
+        echo "Hello World";
+        echo "This is a new line";
+       
+       <h1> 3.1 Name and Id </h1>
+        echo "Catarina <br>";
+        echo "BBCAP22";
+        ?>
+        
+        <h1> 3.2 Display the following message </h1>
+      <?php  echo " Hello world, my name is \"Catarina\"";       
+    ?>
+    <h1> 3.3 get the current date  </h1>
+      <?php  echo date ('d.m.y');?>
+</body>
+</html>
