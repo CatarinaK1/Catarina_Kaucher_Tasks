f416e6e6fa5c8957b82af834af00c75b3058e3ae
CatarinaK1 <111956032+CatarinaK1@users.noreply.github.com>
2023-01-31 11:59:09 +0200

    Update ex1.php

--- ex1.php
+++ ex1.php
@@ -12,14 +12,16 @@
         //Opening tag
         echo "Hello World";
         echo "This is a new line";
-       
+        ?>
        <h1> 3.1 Name and Id </h1>
+       <?php
         echo "Catarina <br>";
         echo "BBCAP22";
         ?>
-        
+
         <h1> 3.2 Display the following message </h1>
-      <?php  echo " Hello world, my name is \"Catarina\"";       
+      <?php 
+       echo " Hello world, my name is \"Catarina\"";
     ?>
     <h1> 3.3 get the current date  </h1>
       <?php  echo date ('d.m.y');?>
