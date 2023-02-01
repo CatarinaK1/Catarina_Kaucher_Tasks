31156bfe91885820bf6219c920b4a287de52b9d1
CatarinaK1 <catarinakaucher@gmail.com>
2023-02-01 11:01:53 +0200

    add

---
+++ ex1.php
@@ -0,0 +1,21 @@
+<?php include 'header.php';?>
+    <h1> My simple PHP script </h1>
+    <?php
+        //Opening tag
+        echo "Hello World";
+        echo "This is a new line";
+        ?>
+       <h1> 3.1 Name and Id </h1>
+       <?php
+        echo "Catarina <br>";
+        echo "BBCAP22";
+        ?>
+
+        <h1> 3.2 Display the following message </h1>
+      <?php 
+       echo " Hello world, my name is \"Catarina\"";
+    ?>
+    <h1> 3.3 get the current date  </h1>
+      <?php  echo date ('d.m.y');?>
+      
+      <?php include 'footer.php';?>
