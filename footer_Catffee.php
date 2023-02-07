eb0b1e73fa9366e33ef13fd943bbb86419f60125
CatarinaK1 <catarinakaucher@gmail.com>
2023-02-07 18:33:48 +0200

    add

--- footer_Catffee.php
+++ footer_Catffee.php
@@ -8,7 +8,7 @@
        
        <?php 
         $path = $_SERVER['PHP_SELF'];
-        echo "Last modified: ".date("F d Y H:i:s.", filemtime("variable.php"));
+        echo "Last modified: ".date("F d Y H:i:s.", filemtime(basename($path)));
          ?>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
