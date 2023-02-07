6d7522d9d1cb576ceecc8b1ec31853ba77593fb1
CatarinaK1 <catarinakaucher@gmail.com>
2023-02-07 11:21:19 +0200

    add

--- action.php
+++ action.php
@@ -1,4 +1,11 @@
-<?php include 'header_Catffee.php';?>
+<?php include 'header_Catffee.php';
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$bdate = $_POST["bdate"];
+$color = $_POST["color"];
 
+echo "<h3> Your name is $fname $lname your birthday is $bdate your fav color is $color</h3>";
+?>
+    
 
 <?php include 'footer_Catffee.php';?>
--- variable.php
+++ variable.php
@@ -7,14 +7,62 @@
     statement to print using tag: Hello …., You are welcome to my site.</h2>
 
 
-    <form action="" method="post">
-    First Name: <input type="text" name="fname"> <br>
-    Last Name: <input type="text" name="lname"> <br>
+    <form action="action.php" method="post">
+    <div class="row">
+    
+    <div class="col">
+    <input type="text" name="fname" placeholder="First Name" class="form-control"> </div>
+    
+    <div class="col">
+    <input type="text" name="lname" placeholder="Last Name" class="form-control"></div>
+    </div>
+
     Birth date: <input type="date" name="bdate"> <br>
     Select fav color: <input type="color" name="color"> <br>
-    <input type="submit" value="Submit">
+    <input type="submit" value="submit">
+    
     </form>
+    <h3> 3 Tables & Variables </h3>
+    <?php 
+        $g1 = 5;
+        $g2 = 4;
+        $g3 = 3;
+       
+        echo "
+        <table class= 'table'>
+        <tr>
+        <th scope='col' > S.N </th><th scope='col'> Name </th><th scope='col'> Grade </th>
+        </tr>
+        <td scope='row'> 1 </td><td> Pekka </td><td> $g1 </td>
+        </tr>
+        <td scope='row'> 1 </td><td> Johanna </td><td> $g2 </td>
+        </tr>
+        <td scope='row'> 1 </td><td> John </td><td> $g3 </td>
+        </tr>
+        </table> ";
+    
+    ?>
+    <h3> 4 Write a PHP script with two string variables. Assign any text to these variables. Join them
+      together.Print the length of the string
+    </h3>
      <?php 
-       echo " Hello world, my name is \"Catarina\""; ?>
+        $txt = "Hello world!";
+         $x = "Hamk";
+         $c = $txt . $x;
+       echo" $txt $x  <br>";
+       echo strlen (" $c ");
+        ?>
+
+    <h3> 5 Write a script to add up the numbers: 298, 234, 46. 
+        Use variables to store these numbers and 
+        echo statement to output your answer. </h3>
+        <?php 
+        $a = 298;
+         $b = 234;
+         $c= 46;
+         $d = $a+ $b+ $c;
+       echo" $d";
+        ?>
+    <h3> 6 Write a PHP script to detect the browser being used to view your pages.</h3>
 
 <?php include 'footer_Catffee.php';?>
